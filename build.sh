dbe81a12d44b1105c771bedc357d6302badb1ce6
bipeens <bipeen.sawant@gslab.com>
2015-10-01 14:38:51 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,9 +5,3 @@ mvn clean install
 
 cp /var/cache/drone/src/github.com/bipeens/SimpleHelloWorld/target/SimpleHelloWorld.war /var/lib/docker
 
-whoami
-sudo service docker start
-sudo docker ps
-
-docker build -t bipeen/docker_drone . 
-docker push bipeen/docker_drone 
